5f57182068c7aadda35115fd5453f1018d546788
eldannyts88 <58126827+eldannyts88@users.noreply.github.com>
2019-12-22 23:52:36 -0300

    Add files via upload

---
+++ Zafary/index.js
@@ -0,0 +1,19 @@
+
+
+
+function ingresar(){
+const input1 = window.document.querySelector('#email');
+const textoInput = input1.value;
+const input2 = window.document.querySelector('#contraseña');
+const textoInput2 = input2.value;
+boton.addEventListener('click', ingresar);
+const boton = window.document.querySelector('#boton');
+if (textoInput === 'danieljaraviana@gmail.com'&& textoInput2 === '12345'){
+    console.log('Puedes ingresar al sistema')
+
+}else{
+    console.log('No puedes ingresar al sistema')
+}
+console.log(botonIng);
+}
+
