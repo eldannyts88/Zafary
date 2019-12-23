7ebbbea70baebcb55a69db91536aff46fe5cb9c9
eldannyts88 <58126827+eldannyts88@users.noreply.github.com>
2019-12-22 23:55:54 -0300

    Update index.js

--- Zafary/index.js
+++ Zafary/index.js
@@ -7,8 +7,7 @@ const input1 = window.document.querySelector('#email');
 const textoInput = input1.value;
 const input2 = window.document.querySelector('#contraseña');
 const textoInput2 = input2.value;
-boton.addEventListener('click', ingresar);
-const boton = window.document.querySelector('#boton');
+
 if (textoInput === 'danieljaraviana@gmail.com'&& textoInput2 === '12345'){
     console.log('Puedes ingresar al sistema')
 
